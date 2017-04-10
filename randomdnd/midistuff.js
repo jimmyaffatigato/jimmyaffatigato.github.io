cc1ebb98adbb5ebd6119450fe7b09c7b18b78a81
jimmyaffatigato <sg7791@gmail.com>
2017-04-09 22:29:39 -0400

    Changed Almost Everything

--- randomdnd/midistuff.js
+++ randomdnd/midistuff.js
@@ -40,6 +40,10 @@ function midiToNoteName(midiNote) {
     }
     return name+oct;
 }
+
+
+var noteBub;
+var midi;
 function openMIDI() {
     if (navigator.requestMIDIAccess) {
         navigator.requestMIDIAccess({
@@ -60,226 +64,33 @@ function openMIDI() {
         if (midi.outputs.size > 0) {
             output = midi.outputs.get(0)
         }
-        var presets = [
-            {
-                num:0,
-                name:"Preset One",
-                waveType: "square",
-                filterAttack: 0,
-                pitchAttack: 0.1,
-                decay: 0.5,
-                filterQ: 20,
-                LFOOnOff : true,
-                lfoSpeed: 0.5,
-                lfoType : "square",
-                calc: function() {}
-            },
-            {
-                num:1,
-                name:"Random",
-                waveType: randomWave(),
-                filterAttack: Math.random(),
-                pitchAttack: randomInt(0,25,"little") / 1000,
-                decay: Math.random() * Math.random(),
-                filterQ: randomInt(1,25),
-                lfoOnOff: randomInt(0,1),
-                lfoSpeed: Math.random() * randomInt(1,10),
-                lfoType: randomWave(),
-                distortionOnOff: randomInt(0,1),
-                distortion: Math.random() * randomInt(0, 3) + 2,
-                calc: function() { }
-            },
-            {
-                num:2,
-                name:"Character",
-                waveType: "sine",
-                filterAttack: 0,
-                pitchAttack: 0,
-                decay: 0,
-                filterQ: 0,
-                lfoOnOff : 0,
-                lfoSpeed: 0,
-                lfoType : "sine",
-                calc: function() {
-                    presets[2].waveType = randomWave();
-                    presets[2].filterAttack = 15/char.abilities.cha
-                    presets[2].pitchAttack = char.abilities.int / 1000
-                    presets[2].decay = char.hp/50
-                    presets[2].filterQ = char.abilities.str
-                    presets[2].lfoOnOff = randomInt(0,1)
-                    presets[2].lfoSpeed = char.abilities.dex
-                    presets[2].lfoType = randomWave()
-                }
-            },
-            {
-                num:3,
-                name:"Environment",
-                waveType: "sine",
-                filterAttack: 0,
-                pitchAttack: 0,
-                decay: 0,
-                filterQ: 0,
-                lfoOnOff : 0,
-                lfoSpeed: 0,
-                lfoType : "sine",
-                calc: function() {
-                    var date = new Date();
-                    presets[3].waveType = randomWave();
-                    presets[3].filterAttack = date.getHours()/24
-                    presets[3].pitchAttack = date.getHours()/24
-                    presets[3].decay = date.getHours()/24
-                    presets[3].filterQ = date.getHours();
-                    presets[3].lfoOnOff = 1;
-                    presets[3].lfoSpeed = date.getHours()/24
-                    presets[3].lfoType = randomWave()
-                }
-            }
-        ]
-        var au = new AudioContext();
-        var notesOn = [];
-        var preset = presets[1]
-        var a = document.getElementById("a")
-        var b = document.getElementById("b")
-        var c = document.getElementById("c")
-        var d = document.getElementById("d")
-        var e = document.getElementById("e")
-        var f = document.getElementById("f")
-        var g = document.getElementById("g")
-        var h = document.getElementById("h")
-        function show() {
-            a.innerHTML = "Wave: " + preset.waveType.capitalize();
-            b.innerHTML = "Filter Attack: " + preset.filterAttack.toFixed(1);
-            c.innerHTML = "Pitch Attack: " + (preset.pitchAttack * 100).toFixed(1);
-            d.innerHTML = "Decay: " + (preset.decay).toFixed(1);
-            console.log(preset.decay)
-            e.innerHTML = "Filter Q: " + preset.filterQ;
-            if (preset.distortionOnOff == 1) {
-                f.innerHTML = "Distortion: x" + parseFloat(preset.distortion).toFixed(1)
-            }
-            if (preset.lfoOnOff == 1) {
-                g.innerHTML = "LFO Speed: " + preset.lfoSpeed.toFixed(1) + "Hz";
-                h.innerHTML = "LFO Wave: " + preset.lfoType.capitalize()
-            }
-        }
-        preset.calc()
-        show()
-        console.log("Preset: ", preset)
-        var waveType = preset.waveType;
-        var filterAttack = preset.filterAttack;
-        var pitchAttack = preset.pitchAttack;
-        var decay = preset.decay;
-        var filterQ = preset.filterQ;
-        var lfoOnOff = preset.lfoOnOff
-        var lfoSpeed = preset.lfoSpeed;
-        var lfoType = preset.lfoType;
-        var bend = 0.0;
-        var distortion = 1
-        if (preset.distortionOnOff == 1) {
-            distortion = preset.distortion
-        };
-        function Voice(note, vel) {
-            this.note = note;
-            var osc = new OscillatorNode(au)
-            var oscGain = new GainNode(au)
-            var lfo = new OscillatorNode(au)
-            var lfoGain = new GainNode(au)
-            var fil = new BiquadFilterNode(au)
-            var env = new GainNode(au)
-            var freq = midiToFreq(note)
-            freq = parseFloat(freq)
-            var amount = freq.toFixed(4) * bend.toFixed(4)
-            var bent = freq + amount;
-            osc.frequency.value = bent - (bent/2)
-            osc.frequency.setTargetAtTime(bent, au.currentTime, pitchAttack)
-            this.getFreq = function() {
-                return [freq, osc.frequency.value];
-            }
-            this.setFreq = function(newFreq) {
-                osc.frequency.setTargetAtTime(newFreq, au.currentTime, 0)
-            }
-            osc.type = waveType;
-            lfo.frequency.value = lfoSpeed;
-            lfo.type = lfoType;
-            fil.type = "lowpass";
-            fil.frequency.value = 0;
-            fil.Q.value = filterQ;
-            fil.frequency.setTargetAtTime(8000, au.currentTime, filterAttack)
-            env.gain.value = vel / 255;
-            env.gain.setTargetAtTime(((vel/255)/1.25),au.currentTime,.25)
-            lfoGain.gain.value = vel / 255
-            oscGain.gain.value = (vel /255) * distortion
-            osc.connect(fil);
-            fil.connect(oscGain)
-            oscGain.connect(env)
-            lfo.connect(lfoGain)
-            lfoGain.connect(oscGain.gain)
-            env.connect(au.destination)
-            osc.start()
-            if (lfoOnOff == 1) {
-                lfo.start()
-            }
-            this.end = function() {
-                env.gain.setTargetAtTime(0, au.currentTime, decay)
-                osc.stop(au.currentTime + 4)
-                if (lfoOnOff == 1) {
-                    lfo.stop(au.currentTime + 4)
-                }
-            }
-        }
         function onMIDIMessage(event) {
             data = event.data;
+            console.log(data)
             switch (data[0]) {
+                //Note Message
                 case 144:
-                    if (data[2] > 0) {
-                        notesOn.push(new Voice(data[1], data[2]));
-                        //console.log(midiToNoteName(data[1]) + " On - V: "+data[2]);
+                    //Note On
+                    if (data[2] > 0){
+                        var newVoice = new Voice(data[1], data[2]);
+                        notesOn.push(newVoice);
+                        newVoice.go()
+                        console.log(notesOn)
                     }
                     else {
-                        for (i=0;i<notesOn.length;i++) {
-                            if (notesOn[i].note == data[1]) {
-                                notesOn[i].end()
-                                notesOn.splice(i, 1)
-                                //console.log(midiToNoteName(data[1]) + " Off")
+                        for(notes=0;notes<notesOn.length;notes++){
+                            if(notesOn[notes].note==data[1]){
+                                notesOn[notes].bye()
+                                notesOn.splice(notes,1)
                             }
                         }
                     }
-                    break;
-                case 176:
-                    bend = data[2];
-                    bend /= 127;
-                    for (i=0;i<notesOn.length;i++) {
-                        var thisFreq = midiToFreq(notesOn[i].note)
-                        var bendAmount = parseFloat(thisFreq).toFixed(3) * bend.toFixed(3)
-                        var newBend = parseFloat(thisFreq) + parseFloat(bendAmount);
-                        newBend = parseFloat(newBend).toFixed(3)
-                        notesOn[i].setFreq(newBend)
-                    };
-                    break;
             }
         }
-        //Update HTML
-        /*var a = document.getElementById("a")
-        var b = document.getElementById("b")
-        var c = document.getElementById("c")
-        var d = document.getElementById("d")
-        var e = document.getElementById("e")
-        var f = document.getElementById("f")
-        var g = document.getElementById("g")
-        a.innerHTML = "Oscillator Type: " + waveType.capitalize();
-        b.innerHTML = "Filter Attack: " + filterAttack.toFixed(1)
-        i.innerHTML = "Pitch Attack: " + pitchAttack.toFixed(3) * 1000;
-        c.innerHTML = "Q: " + filterQ;
-        d.innerHTML = "Decay: " + decay.toFixed(1)
-        e.innerHTML = "Pitch Bend Amount: 0.0"
-        if (lfoOnOff == 1) {
-            f.innerHTML = "LFO Type: " + lfoType.capitalize()
-            g.innerHTML = "LFO Speed: " + lfoSpeed.toFixed(1) + "Hz"
-        }*/
         function sendANote(note) {
             var noteOnMessage = [144, note, 99];
             output.send( noteOnMessage );
             output.send( [128, note, 0], window.performance.now() + 50.0 );  
-            console.log(noteOnMessage);
         }
     }
     function midiFail() {
@@ -287,3 +98,137 @@ function openMIDI() {
     }
 }
 openMIDI();
+
+var presets = [
+    {
+        num:0,
+        name:"pre One",
+        osc1Type: "square",
+        p: {a:[0,0.05],d:[0,0],s:[0,0],r:[0,.5]},
+        f: {a:[8000,0.5],d:[4000,0.5],s:[2500,1],r:[0,0.5]},
+        v: {a:[1,0.25],d:[0.75,0.5],s:[0.7,1],r:[0,0.5]},
+        filterQ: 20,
+        lfo1OnOff: 1,
+        lfo1Speed: 0.5,
+        lfo1Depth: 1,
+        lfo1Type : "square",
+    },
+    {
+        num:1,
+        name:"Random",
+        osc1Type: randomWave(),
+        p: {a:[0,Math.random()*0.05],d:[0,0],s:[0,0],r:[0,.5]},
+        f: {
+            a:[randomInt(0,10000),Math.random()*0.25],
+            d:[randomInt(0,10000),Math.random()*0.25],
+            s:[randomInt(0,10000),Math.random()],
+            r:[0,Math.random()*0.5]},
+        v: {
+            a:[Math.random(),Math.random()*0.25],
+            d:[Math.random(),Math.random()],
+            s:[Math.random(),Math.random()],
+            r:[0,Math.random()*0.25]},
+        filterQ: randomInt(1,25),
+        lfo1OnOff: randomInt(0,1),
+        lfo1Speed: Math.random() * randomInt(1,10),
+        lfo1Depth: Math.random(),
+        lfo1Type: randomWave(),
+    }
+]
+
+
+var au = new AudioContext();
+var notesOn = [];
+var pre = presets[1]
+
+function Voice(note, vel) {
+    var voice = this
+    this.note = note;
+    this.vel = vel/127;
+    this.freq = parseFloat(midiToFreq(note))
+
+    this.osc1 = new OscillatorNode(au)
+    this.osc1Gain = new GainNode(au)
+    this.osc1.type = pre.osc1Type;
+    this.osc1Gain.gain.value = this.vel
+    this.filEnv = new BiquadFilterNode(au)
+    this.filEnv.type = "lowpass";
+    this.filEnv.Q.value = pre.filterQ;
+    this.volEnv = new GainNode(au)
+
+    this.go = function() {
+        this.osc1.frequency.setTargetAtTime(0, au.currentTime, 0)
+        this.osc1.frequency.setTargetAtTime(this.freq,au.currentTime, pre.p.a[1])
+        this.filEnv.frequency.setTargetAtTime(0, au.currentTime, 0)
+        this.filEnv.frequency.setTargetAtTime(pre.f.a[0],au.currentTime, pre.f.a[1])
+        this.filEnv.frequency.setTargetAtTime(pre.f.d[0],(au.currentTime+pre.f.a[1]), pre.f.d[1])
+        this.volEnv.gain.setTargetAtTime(0, au.currentTime, 0)
+        this.volEnv.gain.setTargetAtTime(pre.v.a[0],au.currentTime, pre.v.a[1])
+        this.volEnv.gain.setTargetAtTime(pre.v.d[0],(au.currentTime+pre.v.a[1]), pre.v.d[1])
+    }
+    this.bye = function() {
+        console.log("End" + note)
+        this.filEnv.frequency.setTargetAtTime(pre.f.s[0],au.currentTime, pre.f.s[1])
+        this.filEnv.frequency.setTargetAtTime(pre.f.r[0],(au.currentTime+pre.f.s[1]), pre.f.r[1])
+        this.volEnv.gain.setTargetAtTime(pre.v.s[0],au.currentTime, pre.v.s[1])
+        this.volEnv.gain.setTargetAtTime(pre.v.r[0],(au.currentTime+pre.v.s[1]), pre.v.r[1])
+    }
+
+    //LFO 1
+    this.lfo1 = new OscillatorNode(au);
+    this.lfo1Gain = new GainNode(au);
+    this.lfo1OnOff = pre.lfo1OnOff;
+    this.lfo1.type = pre.lfo1Type;
+    this.lfo1.frequency.value = pre.lfo1Speed;
+    this.lfo1Gain.gain.value = pre.lfo1Depth;
+
+    //Connections
+    this.osc1.connect(this.osc1Gain);
+    this.osc1Gain.connect(this.filEnv)
+    this.filEnv.connect(this.volEnv)
+    this.volEnv.connect(au.destination)
+
+    this.lfo1.connect(this.lfo1Gain)
+    this.lfo1Gain.connect(this.osc1Gain.gain)
+
+    this.osc1.start()
+    if (pre.lfo1OnOff == 1) {
+        this.lfo1.start()
+    }
+}
+
+var a = document.getElementById("a"),
+b = document.getElementById("b"),
+c = document.getElementById("c"),
+d = document.getElementById("d"),
+e = document.getElementById("e"),
+f = document.getElementById("f"),
+g = document.getElementById("g"),
+h = document.getElementById("h"),
+j = document.getElementById("j"),
+k = document.getElementById("k");
+l = document.getElementById("l");
+m = document.getElementById("m");
+n = document.getElementById("n");
+o = document.getElementById("o");
+
+function show() {
+    a.innerHTML = "Wave: " + pre.osc1Type.capitalize();
+    b.innerHTML = "Pitch Attack: " + (pre.p.a[1] * 100).toFixed(1);
+    c.innerHTML = "Filter Attack: " + pre.f.a[1].toFixed(1);
+    d.innerHTML = "Filter Decay: " + pre.f.d[1].toFixed(1);
+    e.innerHTML = "Filter Sustain: " + pre.f.s[1].toFixed(1);
+    f.innerHTML = "Filter Release: " + pre.f.r[1].toFixed(1);
+    g.innerHTML = "Volume Attack: " + pre.v.a[1].toFixed(1);
+    h.innerHTML = "Volume Decay: " + pre.v.d[1].toFixed(1);
+    j.innerHTML = "Volume Sustain: " + pre.v.s[1].toFixed(1);
+    k.innerHTML = "Volume Release: " + (pre.v.r[1]).toFixed(1);
+    l.innerHTML = "Filter Q: " + pre.filterQ;
+    m.innerHTML = "LFO: Off"
+    if (pre.lfo1OnOff == 1) {
+        m.innerHTML = "LFO Speed: " + pre.lfo1Speed.toFixed(1) + "Hz";
+        n.innerHTML = "LFO Wave: " + pre.lfo1Type.capitalize();
+        o.innerHTML = "LFO Depth: " + pre.lfo1Depth.toFixed(1);
+    }
+}
+show()
