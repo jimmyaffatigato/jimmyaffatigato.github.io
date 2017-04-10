bfe8805d80fee02b98d39472305c0323240adb8a
jimmyaffatigato <sg7791@gmail.com>
2017-04-09 23:26:04 -0400

    Fixed More

--- randomdnd/midistuff.js
+++ randomdnd/midistuff.js
@@ -41,18 +41,18 @@ function midiToNoteName(midiNote) {
     return name+oct;
 }
 
-
-var noteBub;
-var midi;
 function openMIDI() {
     if (navigator.requestMIDIAccess) {
         navigator.requestMIDIAccess({
             sysex: false
         }).then(midiContext, midiFail);
     }
+    //I don't really know how this part works
     function midiContext(MIDIAccess) {
         midi = MIDIAccess;
+        //Initialize the input and output variable in this scope
         var input, output;
+        //If there is an input present, default to Port 0; If there are multiple devices, it's still Port 0.
         if (midi.inputs.size > 0) {
             input = midi.inputs.get(0)
             input.onmidimessage = onMIDIMessage;
@@ -61,26 +61,34 @@ function openMIDI() {
         else {
             console.log("No MIDI devices present. Try refreshing (F5)")
         }
+        //MIDI output defaults to Port 0 also
         if (midi.outputs.size > 0) {
             output = midi.outputs.get(0)
         }
+        //Called when a MIDI message is received
+        var notesOn = [];
         function onMIDIMessage(event) {
+            //Message is an array : [channel, note, velocity]
             data = event.data;
-            console.log(data)
             switch (data[0]) {
                 //Note Message
                 case 144:
                     //Note On
                     if (data[2] > 0){
+                        //Create a new voice with the Voice(note, velocity) constructor
                         var newVoice = new Voice(data[1], data[2]);
+                        //Add it to the array of notes that are currently on
                         notesOn.push(newVoice);
                         newVoice.go()
-                        console.log(notesOn)
                     }
+                    //Note Off
                     else {
+                        //Cycles through notes that are on
                         for(notes=0;notes<notesOn.length;notes++){
+                            //If the note value from the array item matches the Note On message, call the bye() method to end the voice
                             if(notesOn[notes].note==data[1]){
                                 notesOn[notes].bye()
+                                //Remove finished voices from the array
                                 notesOn.splice(notes,1)
                             }
                         }
@@ -99,14 +107,17 @@ function openMIDI() {
 }
 openMIDI();
 
+//Array of preset objects
 var presets = [
     {
         num:0,
         name:"pre One",
         osc1Type: "square",
+        //Pitch Envelope
         p: {a:[0,0.05],d:[0,0],s:[0,0],r:[0,.5]},
         f: {a:[8000,0.5],d:[4000,0.5],s:[2500,1],r:[0,0.5]},
         v: {a:[1,0.25],d:[0.75,0.5],s:[0.7,1],r:[0,0.5]},
+        // Envelope values : filter: {attack:[value,time],decay[value,time], ...}
         filterQ: 20,
         lfo1OnOff: 1,
         lfo1Speed: 0.5,
@@ -115,6 +126,7 @@ var presets = [
     },
     {
         num:1,
+        //Random preset randomizes each parameter within certain ranges
         name:"Random",
         osc1Type: randomWave(),
         p: {a:[0,Math.random()*0.05],d:[0,0],s:[0,0],r:[0,.5]},
@@ -122,10 +134,10 @@ var presets = [
             a:[randomInt(0,10000),Math.random()*0.25],
             d:[randomInt(0,10000),Math.random()*0.25],
             s:[randomInt(0,10000),Math.random()],
-            r:[0,Math.random()*0.5]},
+            r:[randomInt(0,10000),Math.random()*0.25]},
         v: {
             a:[Math.random(),Math.random()*0.25],
-            d:[Math.random(),Math.random()],
+            d:[Math.random(),Math.random()*0.25],
             s:[Math.random(),Math.random()],
             r:[0,Math.random()*0.25]},
         filterQ: randomInt(1,25),
@@ -136,11 +148,13 @@ var presets = [
     }
 ]
 
-
+//Create Audio Context
 var au = new AudioContext();
-var notesOn = [];
+
+//Select preset from array
 var pre = presets[1]
 
+//Voice Constructor
 function Voice(note, vel) {
     var voice = this
     this.note = note;
@@ -155,7 +169,7 @@ function Voice(note, vel) {
     this.filEnv.type = "lowpass";
     this.filEnv.Q.value = pre.filterQ;
     this.volEnv = new GainNode(au)
-
+    //Start Envelope
     this.go = function() {
         this.osc1.frequency.setTargetAtTime(0, au.currentTime, 0)
         this.osc1.frequency.setTargetAtTime(this.freq,au.currentTime, pre.p.a[1])
@@ -166,12 +180,18 @@ function Voice(note, vel) {
         this.volEnv.gain.setTargetAtTime(pre.v.a[0],au.currentTime, pre.v.a[1])
         this.volEnv.gain.setTargetAtTime(pre.v.d[0],(au.currentTime+pre.v.a[1]), pre.v.d[1])
     }
+    //Finish Envelope and Stop Oscillator (It is automatically removed)
     this.bye = function() {
-        console.log("End" + note)
+        //Filter Envelope Sustain
         this.filEnv.frequency.setTargetAtTime(pre.f.s[0],au.currentTime, pre.f.s[1])
+        //Filter Envelope Release
         this.filEnv.frequency.setTargetAtTime(pre.f.r[0],(au.currentTime+pre.f.s[1]), pre.f.r[1])
+        //Volume Envelope Sustain
         this.volEnv.gain.setTargetAtTime(pre.v.s[0],au.currentTime, pre.v.s[1])
-        this.volEnv.gain.setTargetAtTime(pre.v.r[0],(au.currentTime+pre.v.s[1]), pre.v.r[1])
+        //Volume Envelope Release
+        this.volEnv.gain.setTargetAtTime(pre.v.r[0],au.currentTime+pre.v.s[1], pre.v.r[1])
+        //Stop Oscillator after 4 seconds
+        this.osc1.stop(au.currentTime + 4)
     }
 
     //LFO 1
@@ -206,29 +226,29 @@ f = document.getElementById("f"),
 g = document.getElementById("g"),
 h = document.getElementById("h"),
 j = document.getElementById("j"),
-k = document.getElementById("k");
-l = document.getElementById("l");
-m = document.getElementById("m");
-n = document.getElementById("n");
+k = document.getElementById("k"),
+l = document.getElementById("l"),
+m = document.getElementById("m"),
+n = document.getElementById("n"),
 o = document.getElementById("o");
 
+//Update HTML
 function show() {
     a.innerHTML = "Wave: " + pre.osc1Type.capitalize();
-    b.innerHTML = "Pitch Attack: " + (pre.p.a[1] * 100).toFixed(1);
-    c.innerHTML = "Filter Attack: " + pre.f.a[1].toFixed(1);
-    d.innerHTML = "Filter Decay: " + pre.f.d[1].toFixed(1);
-    e.innerHTML = "Filter Sustain: " + pre.f.s[1].toFixed(1);
-    f.innerHTML = "Filter Release: " + pre.f.r[1].toFixed(1);
-    g.innerHTML = "Volume Attack: " + pre.v.a[1].toFixed(1);
-    h.innerHTML = "Volume Decay: " + pre.v.d[1].toFixed(1);
-    j.innerHTML = "Volume Sustain: " + pre.v.s[1].toFixed(1);
-    k.innerHTML = "Volume Release: " + (pre.v.r[1]).toFixed(1);
+    c.innerHTML = "Filter Attack: " + pre.f.a[1].toFixed(1)+ "s (" +pre.f.a[0]+" Hz)";
+    d.innerHTML = "Filter Decay: " + pre.f.d[1].toFixed(1)+ "s (" +pre.f.d[0]+" Hz)";
+    e.innerHTML = "Filter Sustain: " + pre.f.s[1].toFixed(1)+ "s (" +pre.f.s[0]+" Hz)";
+    f.innerHTML = "Filter Release: " + pre.f.r[1].toFixed(1)+ "s (" +pre.f.r[0]+" Hz)";
+    g.innerHTML = "Volume Attack: " + pre.v.a[1].toFixed(1)+ "s (" +pre.v.a[0].toFixed(1)+"x)";
+    h.innerHTML = "Volume Decay: " + pre.v.d[1].toFixed(1)+ "s (" +pre.v.d[0].toFixed(1)+"x)";
+    j.innerHTML = "Volume Sustain: " + pre.v.s[1].toFixed(1)+ "s (" +pre.v.s[0].toFixed(1)+"x)";
+    k.innerHTML = "Volume Release: " + (pre.v.r[1]).toFixed(1)+ "s (" +pre.v.r[0].toFixed(1)+"x)";
     l.innerHTML = "Filter Q: " + pre.filterQ;
     m.innerHTML = "LFO: Off"
     if (pre.lfo1OnOff == 1) {
-        m.innerHTML = "LFO Speed: " + pre.lfo1Speed.toFixed(1) + "Hz";
+        m.innerHTML = "LFO Speed: " + pre.lfo1Speed.toFixed(1) + " Hz";
         n.innerHTML = "LFO Wave: " + pre.lfo1Type.capitalize();
-        o.innerHTML = "LFO Depth: " + pre.lfo1Depth.toFixed(1);
+        o.innerHTML = "LFO Depth: " + pre.lfo1Depth.toFixed(1) + "x";
     }
 }
 show()
